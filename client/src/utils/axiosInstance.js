f367bbee3ac7973178b2a80ed16f758c6b57f631
SiddTheCoder <162303130+SiddTheCoder@users.noreply.github.com>
2025-08-13 10:26:40 +0545

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const PRODUCTION_SERVER_URI =
-  "https://e-commerce-fullstack-kinnam-rest.onrender.com/api/v1";
+  "https://e-commerce-fullstack-marto.onrender.com/api/v1";
 
 const DEVELOPMENT_SERVER_URI = "http://localhost:7000/api/v1";
 
